2fce92287361946785e816d005ab22505e4eba64
Marina <marinaberbelp@gmail.com>
2019-11-21 13:38:31 +0100

    cosas

---
+++ readCvs.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+#include "auxFun.h"
+//queremos nnodes, nways
+
+void readFirst(char* name, unsigned long *nnodes,unsigned long *nways);
+void readNodes( char* name, node **nodes, unsigned long nnodes);
